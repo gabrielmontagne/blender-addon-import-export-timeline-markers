74aa937201d5fff8a0b0e095bf35861979a595f2
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-10-17 19:20:04 +0200

    setup for 2.80 upgrade

---
+++ __init__.py
@@ -0,0 +1,12 @@
+bl_id = {
+    'name':  'io m csv',
+    'author': 'Gabriel Montagné Láscaris-Comneno <gabriel@tibas.london>',
+    'version': (0, 0, 1),
+    'blender': (2, 80)
+}
+
+def register():
+    print('RRRRR')
+
+def unregister():
+    print('UUUUR')
